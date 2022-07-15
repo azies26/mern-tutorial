6dbcbf4f687405e8872fd881bc2af042a508c8cd
azies26 <mwahyuazies@gmail.com>
2022-07-15 09:29:51 +0700

    Auth Middleware

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -74,7 +74,7 @@ const loginUser = asyncHandler(async(req, res) => {
 
 // @desc    Get user data
 // @route   GET /api/users/me
-// @access  Public
+// @access  Private
 const getMe = asyncHandler(async(req,res) => {
     const { _id, name,email } = await User.findById(req.user.id)
 
